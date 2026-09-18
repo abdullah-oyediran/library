9a93208a7647f3b17f2073a29b47fb192ed8fd70
abdullah-oyediran <abdullahopeyemi003@gmail.com>
2026-09-18 17:01:20 +0100

    displaying the book once the document loads

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let bookLibrary = document.querySelector('#book-library')
+let formContainer = document.querySelector('#form-container')
+formContainer.style.transform = 'translateY(0px)'
+let header = document.querySelector('#header')
+let titleInput = document.querySelector('#title');
+let authorInput = document.querySelector('#author')
+let pagesInput = document.querySelector('#pages')
+let submitBtn = document.querySelector('#submit-btn')
+
+let books = [];
+
 /*
 h3.book-card
 p.author
@@ -7,11 +18,43 @@ div.footer
     <img alt="Read" src="assets/book-open-check.svg">
 */
 
-let bookLibrary = document.querySelector('#book-library')
-let formContainer = document.querySelector('#form-container')
-formContainer.style.transform = 'translateY(0px)'
-let header = document.querySelector('#header')
-let titleInput = document.querySelector('#title');
-let authorInput = document.querySelector('#author')
-let pagesInput = document.querySelector('#pages')
-let submitBtn = document.querySelector('#submit-btn')
+function displayBooks() {
+    for (let bookItem of books) {
+        let book = document.createElement('div')
+        book.classList.add('book')
+
+        let bookCard = document.createElement('h3')
+        bookCard.textContent = bookItem.title
+        bookCard.classList.add('book-card')
+        book.appendChild(bookCard)
+
+        let author = document.createElement('p')
+        author.classList.add('author')
+        author.textContent = bookItem.author
+        book.appendChild(author)
+
+        let footer = document.createElement('div')
+        footer.classList.add('footer')
+
+        let deleteIcon = document.createElement('img')
+        deleteIcon.src = '../assets/trash.svg'
+        deleteIcon.alt = 'Delete this book'
+        footer.appendChild(deleteIcon)
+
+        let pages = document.createElement('p')
+        pages.classList.add('no-pages')
+        pages.textContent = bookItem.pages
+        footer.appendChild(pages)
+
+        let bookIcon = document.createElement('img')
+        bookIcon.src = '../assets/book-open-check.svg'
+        bookIcon.alt = 'Read'
+        footer.appendChild(bookIcon)
+
+        book.appendChild(footer)
+
+        bookLibrary.appendChild(book)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', displayBooks)
